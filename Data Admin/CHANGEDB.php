20c3cfb81f80566d4116b973df50d350fa701511
Sandra Kuipers <Sandra.Kuipers@tis.edu.mo>
2016-09-07 11:12:16 +0800

    Updated Version and Manifest to v1.0

--- Data Admin/CHANGEDB.php
+++ Data Admin/CHANGEDB.php
@@ -9,27 +9,11 @@ $sql[$count][0]="0.0.00" ;
 $sql[$count][1]="-- First version, nothing to update" ;
 
 
-//v0.0.01
+//v1.0.00
 $count++
-$sql[$count][0]="0.0.01" ;
+$sql[$count][0]="1.0.00" ;
 $sql[$count][1]="
 
 " ;
-
-// CREATE TABLE IF NOT EXISTS `dataAdminImportLog` (
-//   `importLogID` int(8) unsigned zerofill NOT NULL AUTO_INCREMENT,
-//   `gibbonPersonID` int(10) unsigned zerofill NOT NULL,
-//   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-//   `type` varchar(30) NOT NULL,
-//   `success` tinyint(1) NOT NULL,
-//   `importResults` text NOT NULL,
-//   `columnOrder` text NOT NULL,
-//   PRIMARY KEY (`importLogID`)
-// ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;end
-// INSERT INTO `gibbonAction` (`gibbonModuleID`, `name`, `precedence`, `category`, `description`, `URLList`, `entryURL`, `defaultPermissionAdmin`, `defaultPermissionTeacher`, `defaultPermissionStudent`, `defaultPermissionParent`, `defaultPermissionSupport`, `categoryPermissionStaff`, `categoryPermissionStudent`, `categoryPermissionParent`, `categoryPermissionOther`) VALUES ((SELECT gibbonModuleID FROM gibbonModule WHERE name='Data Admin'), 'Manage Imports', 1, 'Import', 'View and run available import actions.', 'import_manage.php,import_run.php,import_run_export.php', 'import_manage.php', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N') ;end
-// INSERT INTO `gibbonPermission` (`permissionID` ,`gibbonRoleID` ,`gibbonActionID`) VALUES (NULL , '1', (SELECT gibbonActionID FROM gibbonAction JOIN gibbonModule ON (gibbonAction.gibbonModuleID=gibbonModule.gibbonModuleID) WHERE gibbonModule.name='Extended Import' AND gibbonAction.name='Manage Imports'));end
-// INSERT INTO `gibbonAction` (`gibbonModuleID`, `name`, `precedence`, `category`, `description`, `URLList`, `entryURL`, `defaultPermissionAdmin`, `defaultPermissionTeacher`, `defaultPermissionStudent`, `defaultPermissionParent`, `defaultPermissionSupport`, `categoryPermissionStaff`, `categoryPermissionStudent`, `categoryPermissionParent`, `categoryPermissionOther`) VALUES ((SELECT gibbonModuleID FROM gibbonModule WHERE name='Data Admin'), 'Import History', 1, 'Reports', 'View a log of import activity.', 'import_history.php,import_history_view.php', 'import_history.php', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N') ;end
-// INSERT INTO `gibbonPermission` (`permissionID` ,`gibbonRoleID` ,`gibbonActionID`) VALUES (NULL , '1', (SELECT gibbonActionID FROM gibbonAction JOIN gibbonModule ON (gibbonAction.gibbonModuleID=gibbonModule.gibbonModuleID) WHERE gibbonModule.name='Extended Import' AND gibbonAction.name='Import History'));end
-
 
 ?>
