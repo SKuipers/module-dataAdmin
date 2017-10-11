54e3c01d8f3bafe85bf3c43f66b2918fac6082dc
Sandra Kuipers <Sandra.Kuipers@tis.edu.mo>
2017-10-11 12:56:15 +0800

    Update URL list for Combine Similar Fields action

--- Data Admin/CHANGEDB.php
+++ Data Admin/CHANGEDB.php
@@ -47,7 +47,7 @@ UPDATE `gibbonAction` SET `category`='Database' WHERE name='Manage Snapshots' AN
 $count++;
 $sql[$count][0]="1.2.02" ;
 $sql[$count][1]="
-INSERT INTO `gibbonAction` (`gibbonModuleID`, `name`, `precedence`, `category`, `description`, `URLList`, `entryURL`, `defaultPermissionAdmin`, `defaultPermissionTeacher`, `defaultPermissionStudent`, `defaultPermissionParent`, `defaultPermissionSupport`, `categoryPermissionStaff`, `categoryPermissionStudent`, `categoryPermissionParent`, `categoryPermissionOther`) VALUES ((SELECT gibbonModuleID FROM gibbonModule WHERE name='Data Admin'), 'Combine Similar Fields', 0, 'Duplication', '', 'duplication_combine.php', 'duplication_combine.php', 'Y', 'N', 'N', 'N', 'N', 'Y', 'N', 'N', 'N') ;end
+INSERT INTO `gibbonAction` (`gibbonModuleID`, `name`, `precedence`, `category`, `description`, `URLList`, `entryURL`, `defaultPermissionAdmin`, `defaultPermissionTeacher`, `defaultPermissionStudent`, `defaultPermissionParent`, `defaultPermissionSupport`, `categoryPermissionStaff`, `categoryPermissionStudent`, `categoryPermissionParent`, `categoryPermissionOther`) VALUES ((SELECT gibbonModuleID FROM gibbonModule WHERE name='Data Admin'), 'Combine Similar Fields', 0, 'Duplication', '', 'duplication_combine.php,duplication_combineConfirm.php', 'duplication_combine.php', 'Y', 'N', 'N', 'N', 'N', 'Y', 'N', 'N', 'N') ;end
 INSERT INTO `gibbonPermission` (`permissionID` ,`gibbonRoleID` ,`gibbonActionID`) VALUES (NULL , '1', (SELECT gibbonActionID FROM gibbonAction JOIN gibbonModule ON (gibbonAction.gibbonModuleID=gibbonModule.gibbonModuleID) WHERE gibbonModule.name='Data Admin' AND gibbonAction.name='Combine Similar Fields'));end
 ";
 
