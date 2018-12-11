834128c0aed010f86e01e43184e5f7366e91fe1d
Sandra Kuipers <sandra@skuipers.com>
2018-12-11 14:16:18 +0800

    Remove old autoloader requirements

--- Data Admin/module.php
+++ Data Admin/module.php
@@ -2,6 +2,3 @@
 
 // Module Functions
 require_once __DIR__ . '/moduleFunctions.php';
-
-// // Add module namespace to Gibbon autoloader
-// $autoloader->addPsr4('Modules\\DataAdmin\\', $_SESSION[$guid]['absolutePath'].'/modules/Data Admin/src/');
