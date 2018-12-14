20ba5690cd723dc0c55b8df5632d9271ac667126
Sandra Kuipers <sandra@skuipers.com>
2018-12-14 09:31:03 +0800

    Move the Importer and ImportType classes to the core, remove files

--- Data Admin/import_manage.php
+++ Data Admin/import_manage.php
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-use Gibbon\Module\DataAdmin\ImportType;
+use Gibbon\Data\ImportType;
 
 use Gibbon\Tables\DataTable;
 use Gibbon\Services\Format;
--- Data Admin/import_run.php
+++ Data Admin/import_run.php
@@ -18,9 +18,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 use Gibbon\Forms\Form;
-use Gibbon\Module\DataAdmin\Importer;
-use Gibbon\Module\DataAdmin\ImportType;
-use Gibbon\Module\DataAdmin\ParseCSV;
+use Gibbon\Data\Importer;
+use Gibbon\Data\ImportType;
 use Gibbon\Tables\DataTable;
 use Gibbon\Domain\DataSet;
 use Gibbon\Services\Format;
